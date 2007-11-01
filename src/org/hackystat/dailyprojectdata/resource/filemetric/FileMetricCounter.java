cdcd527b93601be073585112121a325a0af3993a
Cam Moore <cammymoore07@gmail.com>
2007-11-01 03:20:36 +0000

    Adding FileMetric summary to dailyprojectdata

---
+++ src/org/hackystat/dailyprojectdata/resource/filemetric/FileMetricCounter.java
@@ -0,0 +1,132 @@
+package org.hackystat.dailyprojectdata.resource.filemetric;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.xml.datatype.XMLGregorianCalendar;
+import org.hackystat.sensorbase.client.SensorBaseClient;
+import org.hackystat.sensorbase.client.SensorBaseClientException;
+import org.hackystat.sensorbase.resource.sensordata.jaxb.Property;
+import org.hackystat.sensorbase.resource.sensordata.jaxb.SensorData;
+import org.hackystat.sensorbase.resource.sensordata.jaxb.SensorDataRef;
+import org.hackystat.utilities.time.period.Day;
+import org.hackystat.utilities.tstamp.Tstamp;
+
+/**
+ * Provides a facility for counting the FileMetrics associated with a
+ * SensorData. It only counts the FileMetrics from the 'last' run of the same
+ * tool. This ensures that the FileMetric size is the latest snapshot of the
+ * Project's size.
+ * 
+ * The FileMetricCounter currently only looks at TotalLines.
+ * 
+ * @author Cam Moore
+ * 
+ */
+public class FileMetricCounter {
+
+  private String lastTool;
+  private XMLGregorianCalendar lastTime;
+  private ArrayList<SensorData> fileMetrics;
+  private SensorBaseClient sbClient;
+  private long totalLines;
+
+  /**
+   * Constructs a FileMetricCounter.
+   * 
+   * @param client the SensorBaseClient to use to get the SensorData.
+   */
+  public FileMetricCounter(SensorBaseClient client) {
+    this.lastTool = "";
+    Day first = Day.getInstance(1000, 0, 1); // pick a very early Day
+    this.lastTime = Tstamp.makeTimestamp(first);
+    this.fileMetrics = new ArrayList<SensorData>();
+    this.sbClient = client;
+    this.totalLines = 0;
+  }
+
+  /**
+   * Adds the SensorData to the list of FileMetrics if it is in the last run of the
+   * size counting tool.  Updates the total lines value.  If this sensor data represents
+   * a new run of a size counting tool then the total lines and list of file metrics is 
+   * reset.
+   * @param ref a SensorDataRef, the sensor data to add.
+   */
+  public void add(SensorDataRef ref) {
+    try {
+      SensorData data = sbClient.getSensorData(ref);
+      XMLGregorianCalendar refTime = data.getRuntime();
+      if (Tstamp.equal(lastTime, refTime)) {
+        data = sbClient.getSensorData(ref);
+        if (lastTool.equals(data.getTool())) {
+          // only add data from the same tool.
+          List<Property> props = data.getProperties().getProperty();
+          for (Iterator<Property> iter = props.iterator(); iter.hasNext();) {
+            Property prop = iter.next();
+            if (prop.getKey().equals("TotalLines")) {
+              totalLines += Long.parseLong(prop.getValue());
+            }
+          }
+          fileMetrics.add(data);
+        }
+
+      } else if (Tstamp.greaterThan(refTime, lastTime)) {
+        // newer time so replace everything
+        lastTime = refTime;
+        fileMetrics = new ArrayList<SensorData>();
+        totalLines = 0;
+        lastTool = data.getTool();
+        List<Property> props = data.getProperties().getProperty();
+        for (Iterator<Property> iter = props.iterator(); iter.hasNext();) {
+          Property prop = iter.next();
+          if (prop.getKey().equals("TotalLines")) {
+            totalLines += Long.parseLong(prop.getValue());
+          }
+        }
+        fileMetrics.add(data);
+      }
+    } catch (SensorBaseClientException e) {
+      lastTool = "";
+    }
+
+  }
+
+  /**
+   * @return The total lines as a BigInteger.
+   */
+  public BigInteger getTotalLines() {
+    return BigInteger.valueOf(totalLines);
+  }
+
+  /**
+   * @return The individual FileMetrics SensorDataRefs.
+   */
+  public List<SensorData> getFileMetrics() {
+    return fileMetrics;
+  }
+
+  /**
+   * @return The last time a size counting tool was run.
+   */
+  public XMLGregorianCalendar getLastTime() {
+    return lastTime;
+  }
+
+  /**
+   * @param data a SensorData.
+   * @return The total lines in the given SensorData or 0 if the property "TotalLines"
+   * is undefined.
+   */
+  public BigInteger getTotalLines(SensorData data) {
+    long totalLines = 0;
+    List<Property> props = data.getProperties().getProperty();
+    for (Iterator<Property> iter = props.iterator(); iter.hasNext();) {
+      Property prop = iter.next();
+      if (prop.getKey().equals("TotalLines")) {
+        totalLines += Long.parseLong(prop.getValue());
+      }
+    }
+    return BigInteger.valueOf(totalLines);
+  }
+}
---
+++ src/org/hackystat/dailyprojectdata/resource/filemetric/FileMetricResource.java
@@ -0,0 +1,131 @@
+package org.hackystat.dailyprojectdata.resource.filemetric;
+
+import java.io.StringWriter;
+import java.util.Iterator;
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.hackystat.dailyprojectdata.resource.dailyprojectdata.DailyProjectDataResource;
+import org.hackystat.dailyprojectdata.resource.filemetric.jaxb.FileData;
+import org.hackystat.dailyprojectdata.resource.filemetric.jaxb.FileMetricDailyProjectData;
+import org.hackystat.sensorbase.client.SensorBaseClient;
+import org.hackystat.sensorbase.resource.sensordata.jaxb.SensorData;
+import org.hackystat.sensorbase.resource.sensordata.jaxb.SensorDataIndex;
+import org.hackystat.sensorbase.resource.sensordata.jaxb.SensorDataRef;
+import org.hackystat.utilities.stacktrace.StackTrace;
+import org.hackystat.utilities.tstamp.Tstamp;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.Variant;
+import org.w3c.dom.Document;
+
+/**
+ * Implements the Resource for processing GET
+ * {host}/filemetric/{user}/{project}/{starttime} requests. Requires the
+ * authenticated user to be {user} or else the Admin user for the sensorbase
+ * connected to this service.
+ * 
+ * @author Cam Moore
+ * 
+ */
+public class FileMetricResource extends DailyProjectDataResource {
+
+  /**
+   * The standard constructor.
+   * 
+   * @param context The context.
+   * @param request The request object.
+   * @param response The response object.
+   */
+  public FileMetricResource(Context context, Request request, Response response) {
+    super(context, request, response);
+  }
+
+  /**
+   * Returns an DevTimeDailyProjectData instance representing the FileMetric
+   * associated with the Project data, or null if not authorized.
+   * 
+   * @param variant The representational variant requested.
+   * @return The representation.
+   */
+  @Override
+  public Representation getRepresentation(Variant variant) {
+    if (variant.getMediaType().equals(MediaType.TEXT_XML)) {
+      try {
+        // [1] get the SensorBaseClient for the user making this request.
+        SensorBaseClient client = super.getSensorBaseClient();
+        // [2] get a SensorDataIndex of all sensor data for this Project on
+        // the requested day.
+
+        XMLGregorianCalendar startTime = Tstamp.makeTimestamp(this.timestamp);
+        XMLGregorianCalendar endTime = Tstamp.incrementDays(startTime, 1);
+        SensorDataIndex index = client.getProjectSensorData(authUser, project, startTime, endTime);
+        // [3] look through this index for FileMetric sensor data, and update
+        // the FileMetric counter.
+        FileMetricCounter counter = new FileMetricCounter(client);
+        for (SensorDataRef ref : index.getSensorDataRef()) {
+          if (ref.getSensorDataType().equals("FileMetric")) {
+            counter.add(ref);
+          }
+        }
+        // [4] create and return the FileMetricDailyProjectData
+        FileMetricDailyProjectData fileSize = new FileMetricDailyProjectData();
+        // create the individual FileData elements.
+        List<SensorData> fileInfo = counter.getFileMetrics();
+        for (Iterator<SensorData> iter = fileInfo.iterator(); iter.hasNext();) {
+          SensorData data = iter.next();
+          FileData fileData = new FileData();
+          fileData.setFileUri(data.getResource());
+          fileData.setSizeMetricValue(counter.getTotalLines(data));
+          fileSize.getFileData().add(fileData);
+        }
+         fileSize.setOwner(uriUser);
+         fileSize.setProject(project);
+         fileSize.setUriPattern("**"); // we don't support UriPatterns yet.
+         fileSize.setSizeMetric("LOC");
+         fileSize.setTotalSizeMetricValue(counter.getTotalLines());
+         fileSize.setStartTime(counter.getLastTime());
+         String xmlData = makeFileMetric(fileSize);
+         return super.getStringRepresentation(xmlData);
+      } catch (Exception e) {
+        server.getLogger().warning("Error processing devTime: " + StackTrace.toString(e));
+        return null;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Returns the passed SensorData instance as a String encoding of its XML representation.
+   * @param data The SensorData instance. 
+   * @return The XML String representation.
+   * @throws Exception If problems occur during translation. 
+   */
+  private String makeFileMetric (FileMetricDailyProjectData data) throws Exception {
+    JAXBContext fileMetricJAXB = 
+      (JAXBContext)this.server.getContext().getAttributes().get("FileMetricJAXB");
+    Marshaller marshaller = fileMetricJAXB.createMarshaller(); 
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    dbf.setNamespaceAware(true);
+    DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+    Document doc = documentBuilder.newDocument();
+    marshaller.marshal(data, doc);
+    DOMSource domSource = new DOMSource(doc);
+    StringWriter writer = new StringWriter();
+    StreamResult result = new StreamResult(writer);
+    TransformerFactory tf = TransformerFactory.newInstance();
+    Transformer transformer = tf.newTransformer();
+    transformer.transform(domSource, result);
+    return writer.toString();
+  }
+}
